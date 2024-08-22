6d31339d0e9323a0ddc79698a49817ca706b5cdf
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 17:13:29 +0700

    feat: Add config.go file 🫸🌀✏️📗🐧🐳⬆

---
+++ src/config/config.go
@@ -0,0 +1,12 @@
+package config
+
+type Config struct {
+	Server   ServerConfig
+	Postgres PostgresConfig
+	Redis    RedisConfig
+	Password PasswordConfig
+	Cors     CorsConfig
+	Logger   LoggerConfig
+	Otp      OtpConfig
+	JWT      JWTConfig
+}
