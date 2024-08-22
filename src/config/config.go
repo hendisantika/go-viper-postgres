59c4f9a3195e3cc135053708fc2045169ced4e0c
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 17:15:39 +0700

    feat: Add LoadConfig 🫸🌀✏️📗🐧🐳⬆

--- src/config/config.go
+++ src/config/config.go
@@ -114,3 +114,21 @@ func ParseConfig(v *viper.Viper) (*Config, error) {
 	}
 	return &cfg, nil
 }
+
+func LoadConfig(filename string, fileType string) (*viper.Viper, error) {
+	v := viper.New()
+	v.SetConfigType(fileType)
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Printf("Unable to read config: %v", err)
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+	return v, nil
+}
