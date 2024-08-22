8d85a336dc97dd346f3c6317e4b907fd4a3d6521
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 17:18:25 +0700

    feat: Add InitServer 🫸🌀✏️📗🐧🐳⬆

---
+++ src/api/api.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go-viper-postgres/src/config"
+)
+
+func InitServer() {
+	cfg := config.GetConfig()
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+
+	v1 := r.Group("/api/v1/")
+	{
+		health := v1.Group("/health")
+		routers.Health(health)
+	}
+
+	if err := r.Run(fmt.Sprintf(":%s", cfg.Server.InternalPort)); err != nil {
+		panic(err)
+	}
+}
